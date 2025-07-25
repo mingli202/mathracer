eb444682e7ed2b5bc5873c2df62020a8b94ea8b9
mingli202 <vincentmingli@gmail.com>
2025-07-24 22:59:33 -0400

    feat: Lobby controller

---
+++ backend/server/LobbyController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LobbyController : ControllerBase
+{
+    private readonly Lobbies _lobbies;
+
+    public LobbyController(Lobbies lobbies)
+    {
+        _lobbies = lobbies;
+    }
+
+    [HttpGet("{lobbyId}")]
+    public ActionResult<Lobby> Lobby(string lobbyId)
+    {
+        if (_lobbies.LobbyExists(lobbyId))
+        {
+            return Ok(_lobbies.GetLobby(lobbyId));
+        }
+        return NotFound();
+    }
+
+    [HttpGet("exists/{lobbyId}")]
+    public ActionResult<bool> LobbyExists(string lobbyId)
+    {
+        return _lobbies.LobbyExists(lobbyId);
+    }
+}
