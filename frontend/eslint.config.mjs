4445b986cfaf50835a727fed879b1b11532af819
mingli202 <vincentmingli@gmail.com>
2025-07-28 21:22:14 -0400

    feat: eslint allow unused with pattern

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,21 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error", // or 'warn'
+        {
+          vars: "all", // check all variables
+          args: "after-used", // only unused function args after the last used one
+          ignoreRestSiblings: true, // ignore ...rest destructured siblings
+          varsIgnorePattern: "^_", // ignore any variable starting with _
+          argsIgnorePattern: "^_", // ignore any argument      starting with _
+          caughtErrorsIgnorePattern: "^_", // ignore catch(e)         starting with _
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
