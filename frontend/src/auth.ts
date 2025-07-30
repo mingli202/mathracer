7dc623c11a314b17d848e76064607b69f9e2ceda
mingli202 <vincentmingli@gmail.com>
2025-07-29 21:45:59 -0400

    feat(auth): get public key

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -26,3 +26,24 @@ export async function getCurrentUser(): Promise<User | null> {
 
   return user;
 }
+
+export async function getPublicKey(): Promise<CryptoKey> {
+  const key: CryptoKey = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/auth/key`,
+  )
+    .then((key) => key.text())
+    .then((base64) => {
+      const spki = Uint8Array.from(atob(base64), (t) => t.charCodeAt(0));
+
+      // RSA algorithms always include a hash function
+      // so we need to specify which one we use (SHA-256 here)
+      return crypto.subtle.importKey(
+        "spki",
+        spki,
+        { name: "RSA-OAEP", hash: "SHA-256" },
+        false,
+        ["encrypt"],
+      );
+    });
+  return key;
+}
