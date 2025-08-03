75742fb338b5435c3ff06085ed1c47255e26ec50
mingli202 <vincentmingli@gmail.com>
2025-08-03 14:04:11 -0400

    some logs and doc strings I guess

--- backend/server/Services/TokenService.cs
+++ backend/server/Services/TokenService.cs
@@ -9,7 +9,7 @@ public class Token
     public Token(string user)
     {
         this.dateIssued = DateTime.Now;
-        this.expiration = DateTime.Now.AddMinutes(10);
+        this.expiration = DateTime.Now.AddSeconds(10);
         this.user = user;
     }
 
@@ -29,24 +29,41 @@ public interface ITokenService
 public class TokenService : ITokenService
 {
     private readonly EncryptionService _encryptionService;
+    private readonly LoggingService _logger;
 
-    public TokenService(LoggingService logger)
+    public TokenService(LoggingService logger, EncryptionService encryptionService)
     {
         this._encryptionService = new EncryptionService(logger);
+        this._logger = logger;
     }
 
+    /// <summary>
+    /// Signs a token and returns the encrypted token string.
+    /// </summary>
+    /// <param name="token">The token to sign.</param>
+    /// <returns>The encrypted token string.</returns>
     public string Sign(Token token)
     {
         string encryptedToken = this._encryptionService.Encrypt(token.ToString());
         return encryptedToken;
     }
 
+    /// <summary>
+    /// Generates a new signed token for a user.
+    /// </summary>
+    /// <param name="user">The user to generate the token for.</param>
+    /// <returns>The encrypted token string.</returns>
     public string NewSignedToken(string user)
     {
         Token token = new Token(user);
         return this.Sign(token);
     }
 
+    /// <summary>
+    /// Validates a token string and returns a new token if valid, otherwise null.
+    /// </summary>
+    /// <param name="tokenString">The token string to validate.</param>
+    /// <returns>The new token if valid, otherwise null.</returns>
     public Token? Validate(string? tokenString)
     {
         if (tokenString == null)
@@ -70,8 +87,13 @@ public class TokenService : ITokenService
 
         if (token.expiration < DateTime.Now)
         {
+            this._logger.Log(Severity.Debug, $"ValidateToken failed, token expired at {token.expiration}", token);
             return null;
         }
+
+        Token newToken = new Token(token.user);
+
+        this._logger.Log(Severity.Debug, "Validate token succeeded, generating new token", newToken);
 
         return token;
     }
