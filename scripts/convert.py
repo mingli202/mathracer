83f0eae963b5c92017aa3b7eb9959fea02f31939
mingli202 <vincentmingli@gmail.com>
2025-08-13 22:29:14 -0400

    we actually need python 3.12 for it to work fuck man

--- scripts/convert.py
+++ scripts/convert.py
@@ -2,9 +2,25 @@ from models import MyModel
 import tensorflowjs as tfjs
 import keras
 from pathlib import Path
+import argparse
+
+
+def convert(model_name: str):
+    print(f"Converting {model_name}")
+
+    path = Path(f"./artifacts/{model_name}.keras")
+    model = keras.models.load_model(path)
+
+    out_path = Path(f"../public/models/{model_name}")
+    out_path.mkdir(parents=True, exist_ok=True)
+    tfjs.converters.save_keras_model(model, out_path)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="all")
+    args = parser.parse_args()
+
     models = MyModel()
 
     models_dict = {
@@ -16,14 +32,11 @@ def main():
         "keras_tutorial": models.keras_tutorial,
     }
 
-    for key in models_dict.keys():
-        path = Path(f"./artifacts/{key}.keras")
-        print(path)
-        model = keras.models.load_model(path)
-
-        out_path = Path(f"../public/models/{key}")
-        out_path.mkdir(parents=True, exist_ok=True)
-        tfjs.converters.save_keras_model(model, out_path)
+    if args.model == "all":
+        for key in models_dict.keys():
+            convert(key)
+    else:
+        convert(args.model)
 
 
 if __name__ == "__main__":
