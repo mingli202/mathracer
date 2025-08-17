790f8506ebdddef106d638f2936ba3bdcb927adb
mingli202 <vincentmingli@gmail.com>
2025-08-16 20:05:57 -0400

    feat: added model from tfjs tutorial

---
+++ scripts/models/tfjsTutorial.ts
@@ -0,0 +1,33 @@
+import type { MnistData } from "../data";
+import { Model } from "../model";
+import * as tf from "@tensorflow/tfjs-node";
+
+export class TfjsTutorial extends Model {
+  constructor(data: MnistData) {
+    const layers = [
+      tf.layers.inputLayer({ inputShape: [28, 28, 1] }),
+      tf.layers.conv2d({
+        filters: 8,
+        kernelSize: 5,
+        activation: "relu",
+        kernelInitializer: "varianceScaling",
+      }),
+      tf.layers.maxPool2d({ poolSize: 2, strides: 2 }),
+      tf.layers.conv2d({
+        filters: 15,
+        kernelSize: 5,
+        activation: "relu",
+        kernelInitializer: "varianceScaling",
+      }),
+      tf.layers.maxPool2d({ poolSize: 2, strides: 2 }),
+      tf.layers.flatten(),
+      tf.layers.dense({
+        units: 10,
+        activation: "softmax",
+        kernelInitializer: "varianceScaling",
+      }),
+    ];
+
+    super(layers, "tfjsTutorial", data);
+  }
+}
