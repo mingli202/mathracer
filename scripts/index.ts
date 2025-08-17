f3a7e14b0294348f1ae9ef0aed11cdef49ae4db2
mingli202 <vincentmingli@gmail.com>
2025-08-16 22:56:11 -0400

    actually multithreding

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,74 +1,56 @@
+import { Args } from "./args";
 import { MnistData } from "./data";
-import { Models } from "./models";
+import type { Models } from "./models";
 
-async function main(args: string[]) {
-  const data = new MnistData();
+async function main(argsArr: string[]) {
+  const args = new Args(argsArr);
 
-  // to not call the constructor
-  let Model = Models.Mini;
-  let batchSize: number | undefined;
-  let maxEpochs: number | undefined;
+  if (args.model) {
+    const data = new MnistData();
+    const model = new args.model(data, undefined, {
+      batchSize: args.batchSize,
+      epochs: args.maxEpochs,
+    });
+    await model.fit();
 
-  if (args.length > 0) {
-    if (args.includes("--help")) {
-      printHelp();
-      process.exit(1);
+    const [loss, accuracy] = model.evaluate();
+
+    console.log("accuracy:", accuracy.arraySync());
+    console.log("loss:", loss.arraySync());
+
+    model.saveModel(accuracy, loss);
+  } else {
+    const modelsBatch = [
+      ["LeNet", "Mini", "TfjsTutorial"],
+      ["ChatGpt5", "MobileNetMini", "KerasTutorial"],
+    ] as const;
+
+    const nWorkers = modelsBatch.length;
+    const workers: Promise<Worker>[] = [];
+
+    for (const i of Array(nWorkers).keys()) {
+      console.log("Spawned worker ", i);
+
+      workers.push(
+        new Promise((resolve) => {
+          const worker = new Worker("./train.ts", {
+            preload: ["./data.ts", "./models/index.ts"],
+          });
+          worker.postMessage(modelsBatch[i]);
+          worker.onmessage = (event: MessageEvent) => {
+            if (event.data === "COMPLETED") {
+              resolve(worker);
+            }
+          };
+        }),
+      );
     }
 
-    const argsStr = ` ${args.join(" ")} `;
-
-    const modelArg = argsStr.match(/ --model (\w+) /);
-    const batchSizeArg = argsStr.match(/ --batch-size (\d+) /);
-    const maxEpochsArg = argsStr.match(/ --max-epochs (\d+) /);
-
-    if (modelArg) {
-      const modelArgStr = modelArg[1];
-
-      if (!modelArgStr || !(modelArgStr in Models)) {
-        printHelp();
-        process.exit(1);
-      }
-
-      Model = Models[modelArgStr as keyof typeof Models];
-    }
-
-    if (batchSizeArg) {
-      batchSize = Number(batchSizeArg[1]);
-    }
-
-    if (maxEpochsArg) {
-      maxEpochs = Number(maxEpochsArg[1]);
+    const completedWorkers = await Promise.all(workers);
+    for (const worker of completedWorkers) {
+      worker.terminate();
     }
   }
-
-  const model = new Model(data, undefined, {
-    batchSize,
-    epochs: maxEpochs,
-  });
-  await model.fit();
-
-  const [loss, accuracy] = model.evaluate();
-
-  console.log("accuracy:", accuracy.arraySync());
-  console.log("loss:", loss.arraySync());
-
-  model.saveModel(accuracy, loss);
-}
-
-function printHelp() {
-  const lines: string[] = [
-    "Usage: bun index.ts [--model <model>] [--max-epochs <epochs>] [--batch-size <size>]",
-    "\nOptions:",
-    "    --model <model>",
-    "        Model to train, defaults to Mini. Avaible models: ",
-    `        ${Object.keys(Models).join(", ")}`,
-    "    --max-epochs <epochs>",
-    "        Train for at most <epochs>. Defaults to 60.",
-    "    --batch-size <size>",
-    "        Training batch size. Default varies depending on the model.",
-  ];
-
-  console.log(lines.join("\n"));
 }
 
 main(process.argv.slice(1));
---
+++ scripts/train.ts
@@ -0,0 +1,22 @@
+import { MnistData } from "./data";
+import { Models } from "./models";
+
+declare var self: Worker;
+
+self.onmessage = async (event: MessageEvent) => {
+  const models: (keyof typeof Models)[] = event.data;
+
+  const data = new MnistData();
+
+  for (const m of models) {
+    const model = new Models[m](data);
+    await model.fit();
+
+    const [loss, accuracy] = model.evaluate();
+
+    console.log("accuracy:", accuracy.arraySync());
+    console.log("loss:", loss.arraySync());
+
+    model.saveModel(accuracy, loss);
+  }
+};
